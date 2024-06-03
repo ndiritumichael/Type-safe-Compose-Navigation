8c8d9a198cdb96f5aae9c4e9c22a463097587d83
Ndiritu <ndiritumichael5@gmail.com>
2024-06-03 11:03:55 +0300

    fix padding for status bar

--- app/src/main/java/com/example/typesafecomposenavigation/MainActivity.kt
+++ app/src/main/java/com/example/typesafecomposenavigation/MainActivity.kt
@@ -4,25 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.typesafecomposenavigation.ui.theme.TypeSafeComposeNavigationTheme
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,64 +24,33 @@ class MainActivity : ComponentActivity() {
 
 
         setContent {
+            val statusBarHeight = WindowInsets.statusBars
 
             val navController = rememberNavController()
             TypeSafeComposeNavigationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {
-                    RecipeBottomBar(navController = navController)
-                }) { innerPadding ->
-                   AppNavigation(navController = navController , startDestination = RecipeScreens.Profile, modifier = Modifier.padding(innerPadding))
+                Surface {
+
+
+                    Scaffold(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(
+                            top = statusBarHeight
+                                .asPaddingValues()
+                                .calculateTopPadding()
+                        )
+                        .consumeWindowInsets(WindowInsets.statusBars)
+                        .statusBarsPadding(),
+                        bottomBar = {
+                            RecipeBottomBar(navController = navController)
+                        }) { innerPadding ->
+                        AppNavigation(
+                            navController = navController,
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TypeSafeComposeNavigationTheme {
-        Greeting("Android")
-    }
-}
-
-
-@Composable
-fun ProgressBar(modifier: Modifier) {
-
-    var state by remember {
-        mutableFloatStateOf(0f)
-    }
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-
-
-
-
-
-    }
-
-
-    LaunchedEffect(key1 = true) {
-        while(state<1.1f){
-            if(state >1f){
-                state = 0f
-            }
-
-            delay(1000)
-            state += 0.1f
-        }
-
-
-    }
-
-
-
-}

a5f391b9f2d9c0a569aa13f9b4480bebc87ee994
Ndiritu <ndiritumichael5@gmail.com>
2024-06-03 10:53:40 +0300

    implement recipe detail viewmodel to demonstrate getting route arguments in viewmodel

---
+++ app/src/main/java/com/example/typesafecomposenavigation/screens/recipedetail/RecipeDetailViewModel.kt
@@ -0,0 +1,61 @@
+package com.example.typesafecomposenavigation.screens.recipedetail
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.navigation.toRoute
+import com.example.typesafecomposenavigation.RecipeScreens
+import com.example.typesafecomposenavigation.data.RecipeRepository
+import com.example.typesafecomposenavigation.model.RecipeModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class RecipeDetailViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
+
+
+    /**
+     *   You can also access the arguments directly from the [SavedStateHandle].
+     */
+    private val detailRoute: RecipeScreens.RecipeDetails = savedStateHandle.toRoute()
+
+
+    private val _recipeDetails = MutableStateFlow<RecipeDetailState>(RecipeDetailState.Idle)
+
+    val recipeDetails = _recipeDetails.asStateFlow()
+
+
+    fun getRecipeById(id: Int) {
+
+
+        // In a production app set up your repository as a constructor argument
+        RecipeRepository.getRecipeById(id)?.let {
+
+            _recipeDetails.value = RecipeDetailState.Success(it)
+
+        } ?: {
+            _recipeDetails.value = RecipeDetailState.Error("Recipe not found")
+        }
+
+
+    }
+
+    fun toggleFavorite(id: Int, isFavorite: Boolean) {
+
+        if (isFavorite) {
+            RecipeRepository.removeFromFavorites(id)
+        } else {
+            RecipeRepository.addToFavorites(id)
+        }
+
+
+    }
+
+
+}
+
+sealed interface RecipeDetailState {
+    data object Idle : RecipeDetailState
+    data object Loading : RecipeDetailState
+    data class Error(val message: String) : RecipeDetailState
+    data class Success(val recipe: RecipeModel) : RecipeDetailState
+
+}

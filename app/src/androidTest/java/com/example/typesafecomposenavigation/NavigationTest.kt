9bd3f01ea27d9f0bb267fd69c76aa232389c223e
Ndiritu <ndiritumichael5@gmail.com>
2024-06-13 12:28:42 +0300

    navigation testing sample

---
+++ app/src/androidTest/java/com/example/typesafecomposenavigation/NavigationTest.kt
@@ -0,0 +1,32 @@
+package com.example.typesafecomposenavigation
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.navigation.testing.invoke
+import androidx.navigation.toRoute
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.typesafecomposenavigation.ui.navigation.RecipeDestinations
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Assert.*
+
+
+@RunWith(AndroidJUnit4::class)
+class NavigationTest {
+
+
+
+
+    @Test
+    fun testNavigation() {
+       val detail = RecipeDestinations.RecipeDetails(2)
+
+        val savedStateHandle = SavedStateHandle.Companion.invoke(detail)
+
+        val details : RecipeDestinations.RecipeDetails = savedStateHandle.toRoute()
+
+        println(" the test is complete  the details is $details")
+        assertEquals(details.hashCode(),detail.hashCode())
+    }
+
+
+}

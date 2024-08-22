e5e6ac82a313d11d1bd8cc5095165c65dc307208
Ndiritu <ndiritumichael5@gmail.com>
2024-08-22 13:30:28 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.typesafecomposenavigation"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.typesafecomposenavigation"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
